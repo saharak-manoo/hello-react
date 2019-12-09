eaad261e8a064c747ef9ca733eb6c1d7e33d1228
Saharak <Saharakmanoo@outlook.com>
2019-12-09 21:04:26 +0700

    [Added] pg

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const path = require('path');
+const { Client } = require('pg');
+const connectionString = 'postgres://saharak:@localhost:5432/ruby_on_rails_dev';
+
+const client = new Client({
+	connectionString: connectionString
+});
+
+client.connect();
 
 const app = express();
 
@@ -7,6 +15,8 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.get('*', (req, res) => {
+	console.log(res);
+	console.log(req);
 	res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
