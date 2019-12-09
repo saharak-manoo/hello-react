f4650481a645c0582e22e2eda2ab6820bd196c35
Saharak <Saharak@bananacoding.com>
2019-12-09 19:20:01 +0700

    [Setup] projects

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class About extends Component {
+	render() {
+		return (
+			<div>
+				<h1>About Page</h1>
+			</div>
+		);
+	}
+}
+
+export default About;
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import React, { PropTypes, Component } from 'react';
+import './style.css';
+
+class NotFound extends Component {
+	render() {
+		return (
+			<div>
+				<h1>404 : Not Found :(</h1>
+			</div>
+		);
+	}
+}
+
+export default NotFound;
